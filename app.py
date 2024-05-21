d06c3f14b360940ec8a68775a226855b593c7360
tri_th.hcm <tranhtri.hcm@vnpt.vn>
2024-05-21 19:12:41 +0700

    fix bug code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, make_response
 from test_denoise import img2tensor, predict_single_image
 from basicsr.models import create_model
 from basicsr.utils.options import parse
@@ -6,12 +6,14 @@ from basicsr.utils import imwrite
 from datetime import datetime
 import numpy as np
 import os
+from flask_cors import CORS
 from PIL import Image
 
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+CORS(app)
 
 opt_path = 'options/NAFNet-width32.yml'
 opt = parse(opt_path, is_train=False)
@@ -22,24 +24,42 @@ NAFNet = create_model(opt)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-@app.route('/upload-image', methods=['POST'])
+@app.route('/upload-image', methods=[ 'OPTIONS','POST', 'GET'])
 def upload_image():
+
+    # if request.method == 'GET':
+    #     return 'GET request received'
+    if request.method == 'OPTIONS':
+        # Respond to preflight request
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
 
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
 
     file = request.files['file']
+    print(file.filename)
 
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
     if file:
         file_extension = os.path.splitext(file.filename)[1].lower()
+
         # Read the image file into a PIL Image
         image = Image.open(file.stream)
 
         # Convert the image to a NumPy array
         image_np = np.array(image)
+
+        # Check if the image has 4 channels (RGBA)
+        if len(image_np.shape) == 3 and image_np.shape[2] == 4:
+            # Convert RGBA to RGB by discarding the alpha channel
+            image_np = image_np[:, :, :3]
+
 
         # Convert the NumPy array to a tensor
         tensorImg = img2tensor(image_np)
@@ -59,4 +79,4 @@ def upload_image():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- test_denoise.py
+++ test_denoise.py
@@ -1,16 +1,8 @@
-import torch
 
-from basicsr.models import create_model
 from basicsr.utils import img2tensor as _img2tensor, tensor2img, imwrite
-from basicsr.utils.options import parse
 import numpy as np
-import cv2
 import matplotlib.pyplot as plt
 
-def imread(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img
 def img2tensor(img, bgr2rgb=False, float32=True):
     img = img.astype(np.float32) / 255.
     return _img2tensor(img, bgr2rgb=bgr2rgb, float32=float32)
@@ -56,17 +48,5 @@ def predict_single_image(model, img):
     sr_img = tensor2img([visuals['result']])
     return sr_img
 
-opt_path = 'options/NAFNet-width32.yml'
-opt = parse(opt_path, is_train=False)
-opt['dist'] = False
-NAFNet = create_model(opt)
-
-input_path = 'images/noisy.png'
-output_path = 'images/noisy-output.png'
-
-img_input = imread(input_path)
-inp = img2tensor(img_input)
-single_image_inference(NAFNet, inp, output_path)
-img_output = imread(output_path)
 
 
